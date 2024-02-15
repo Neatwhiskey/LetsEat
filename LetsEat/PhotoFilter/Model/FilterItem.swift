b33a0c4ddb7c9cefd989df4a72e18f6b68e5fa41
Neatwhiskey <44437144+Neatwhiskey@users.noreply.github.com>
2024-02-15 14:26:48 +0100

    Set up the photo filter scene

---
+++ LetsEat/PhotoFilter/Model/FilterItem.swift
@@ -0,0 +1,18 @@
+//
+//  FilterItem.swift
+//  LetsEat
+//
+//  Created by Jamaaldeen Opasina on 13/02/2024.
+//
+
+import Foundation
+
+struct FilterItem{
+    let filter: String?
+    let name : String?
+    
+    init(dict: [String: String]){
+        self.filter = dict["filter"]
+        self.name = dict["name"]
+    }
+}
