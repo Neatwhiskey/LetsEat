2e546a0bcd6f7589564bafc8f8c8be3340dd4fe5
Neatwhiskey <44437144+Neatwhiskey@users.noreply.github.com>
2024-02-15 14:26:48 +0100

    Set up the photo filter scene

---
+++ LetsEat/PhotoFilter/Model/FilterItem.swift
@@ -0,0 +1,8 @@
+//
+//  FilterItem.swift
+//  LetsEat
+//
+//  Created by Jamaaldeen Opasina on 13/02/2024.
+//
+
+import Foundation
---
+++ LetsEat/PhotoFilter/View/FilterCell.swift
@@ -0,0 +1,12 @@
+//
+//  FilterCell.swift
+//  LetsEat
+//
+//  Created by Jamaaldeen Opasina on 13/02/2024.
+//
+
+import UIKit
+
+class FilterCell: UICollectionViewCell {
+    
+}
